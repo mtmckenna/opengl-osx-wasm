afb48fc48809d30e6a9aba7c3c2a2d11fbb7faf0
Matt McKenna <matt@mtmckenna.com>
2019-04-07 21:31:23 -0700

    remove extraneous mime types

--- scripts/server.js
+++ scripts/server.js
@@ -13,17 +13,7 @@ http.createServer(function (request, response) {
       ".html": "text/html",
       ".js": "text/javascript",
       ".css": "text/css",
-      ".json": "application/json",
       ".png": "image/png",
-      ".jpg": "image/jpg",
-      ".gif": "image/gif",
-      ".wav": "audio/wav",
-      ".mp4": "video/mp4",
-      ".woff": "application/font-woff",
-      ".ttf": "application/font-ttf",
-      ".eot": "application/vnd.ms-fontobject",
-      ".otf": "application/font-otf",
-      ".svg": "application/image/svg+xml",
       ".wasm": "application/wasm",
   };
 
